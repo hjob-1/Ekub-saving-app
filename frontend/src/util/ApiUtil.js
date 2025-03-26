1558d8c6a4fc193223efb0a426e0ec3f5f2fbc99
hjob-1 <hjob12tadele@gmail.com>
2025-03-26 17:18:10 -0400

    api modified

--- frontend/src/util/ApiUtil.js
+++ frontend/src/util/ApiUtil.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const frameToken = (token) => {
   return `Bearer ${token}`;
